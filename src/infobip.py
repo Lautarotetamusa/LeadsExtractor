74b5eac554755de646a65bafc5cd00eea449c9a5
lautarotetamusa <lautarotetamusa@gmail.com>
2024-03-19 23:03:30 -0300

    update and create people in batch infobip

--- src/infobip.py
+++ src/infobip.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import json
 import requests
 from dotenv import load_dotenv
 import os
@@ -92,13 +94,59 @@ def search_person(logger: Logger, phone: str) -> None | Lead:
 def update_person(logger: Logger, id: int, payload: dict):
     logger.debug(f"Actualizando persona {id}")
 
-    res = requests.put(API_URL, params={"id": id}, json=payload, headers=HEADERS)
-    if not res.ok:
-        logger.error("Error en la request: " + str(res.status_code))
-        logger.error(res.json())
-        return
+    try:
+        res = requests.put(API_URL, params={"id": id}, json=payload, headers=HEADERS)
+        if not res.ok:
+            logger.error("Error en la request: " + str(res.status_code))
+            logger.error(res.json())
+            return False
+    except Exception as e:
+        logger.error("Error cargando lead"+str(e))
+        return False
 
     logger.success(f"Persona {id} actualizada correctamente")
+    return True
+
+def add_fecha_lead(logger: Logger, leads: list[Lead]) -> bool | list[dict]:
+    persons = []
+    phones = []
+    for lead in leads:
+        #Si intentamos actualizar una persona dos veces en el mismo payload dice que no existe el numero aunque si exista
+        #De este modo eliminamos duplicados y solucionamos el problema
+        if lead.telefono in phones: #ERROR muy raro de infobip
+            continue
+
+        person = {
+            "query": {
+                "identifier": lead.telefono[1:], #Sacamos el +
+                "type": "PHONE"
+            },
+            "update": {
+                "customAttributes":{
+                    "fecha_lead": lead.fecha_lead
+                }
+            }
+        }
+        persons.append(person)
+        phones.append(lead.telefono)
+    payload = {'people': persons}
+    print(payload)
+
+    try:
+        res = requests.patch(API_URL+'/batch', json=payload, headers=HEADERS)
+        if not res.ok:
+            logger.error("Error en la request: " + str(res.status_code))
+            logger.error(res.json())
+            return False
+    except Exception as e:
+        logger.error("Error cargando lead"+str(e))
+        return False
+
+    #Cuando sale todo bien devuelve null
+    if res.json() == None:
+        return []
+    
+    return res.json()['results']
 
 #Si valid_number es True, no se parseara el numero para no generar problemas
 def create_person(logger: Logger, lead: Lead):
@@ -116,22 +164,72 @@ def create_person(logger: Logger, lead: Lead):
             "contacted": False,
             "fuente": lead.fuente,
             "asesor_name": lead.asesor['name'],
-            "asesor_phone": lead.asesor['phone']
+            "asesor_phone": lead.asesor['phone'],
+            "fecha_lead": lead.fecha_lead
         },
         "contactInformation": {},
         "tags": "Seguimientos"
     }
-    if lead.telefono != '':
+    if lead.telefono != '' and lead.telefono != None:
         payload["contactInformation"]['phone']= [{
             "number": lead.telefono
         }]
-    if lead.email != '':
+    if lead.email != '' and lead.email != None:
         payload["contactInformation"]['email'] = [{
             "address": lead.email
         }]
+    print("PAYLOAD:")
+    print(json.dumps(payload, indent=4))
 
     try:
         res = requests.post(API_URL, json=payload, headers=HEADERS)
+        if not res.ok:
+            logger.error("Error en la request: " + str(res.status_code))
+            logger.error(res.json())
+            logger.error(payload)
+            return False
+    except Exception as e:
+        logger.error("Error cargando lead"+str(e))
+        return False
+
+    logger.success("Lead cargada correctamente")
+    return True
+
+#Si valid_number es True, no se parseara el numero para no generar problemas
+def create_persons(logger: Logger, leads: list[Lead]):
+    persons = []
+    for lead in leads:
+        person = {
+            "firstName": lead.nombre,
+            "lastName": "",
+            "type": "LEAD", #Para que los leads entren en el flow
+            "customAttributes": {
+                "prop_link": lead.propiedad['link'],
+                "prop_precio": str(lead.propiedad['precio']),
+                "prop_ubicacion": lead.propiedad['ubicacion'],
+                "prop_titulo": lead.propiedad['titulo'],
+                "contacted": False,
+                "fuente": lead.fuente,
+                "asesor_name": lead.asesor['name'],
+                "asesor_phone": lead.asesor['phone'],
+                "fecha_lead": lead.fecha_lead
+            },
+            "contactInformation": {},
+            "tags": "Seguimientos"
+        }
+        if lead.telefono != '':
+            person["contactInformation"]['phone']= [{
+                "number": lead.telefono
+            }]
+        if lead.email != '':
+            person["contactInformation"]['email'] = [{
+                "address": lead.email
+            }]
+        persons.append(person)
+    payload = {'people': persons}
+
+    try:
+        res = requests.post(API_URL+'/batch', json=payload, headers=HEADERS)
         if not res.ok:
             logger.error("Error en la request: " + str(res.status_code))
             logger.error(res.json())
@@ -141,5 +239,4 @@ def create_person(logger: Logger, lead: Lead):
         logger.error("Error cargando lead"+str(e))
         return False
 
-    logger.success("Lead cargada correctamente")
-    return True
+    return res.json()['results']
