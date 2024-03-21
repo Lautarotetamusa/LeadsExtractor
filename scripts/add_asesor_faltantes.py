229ede4e24c60e3a8afdc8c797fd3b26180e5a7d
lautarotetamusa <lautarotetamusa@gmail.com>
2024-03-21 16:53:05 -0300

    Agregar asesores faltantes

--- scripts/add_asesor_faltantes.py
+++ scripts/add_asesor_faltantes.py
@@ -4,8 +4,10 @@
 import sys
 import json
 import urllib.parse
+
 sys.path.append('.')
 
+from src.whatsapp import Whatsapp
 from src.logger import Logger
 from src.logger import Logger
 from src.sheets import Sheet
@@ -14,24 +16,34 @@ import src.infobip as infobip
 if __name__ == "__main__":
     logger = Logger("Update persons")
     sheet = Sheet(logger, "mapping.json")
+    wpp = Whatsapp()
 
-    json_filter = {"customAttributes": {"asesor_phone": None}}
+    #Buscamos las personas que no tengan numero o nombre del asesor asignado
+    json_filter = {"#or": [
+        {"customAttributes": {"asesor_phone": None}}, 
+        {"customAttributes": {"asesor_name": None}}
+    ]}
     filtro = urllib.parse.quote(json.dumps(json_filter))
-    persons = infobip.get_all_with_filter(logger, filtro)
-    logger.debug(f"Personas sin asesor: {len(persons)}")
     
     rows = sheet.get('Asesores!A2:C25')
     activos = [row for row in rows if row[2] == "Activo"]
     logger.debug(f"Activos: {activos}")
 
     asesor_i = 0
-    for person in persons:
-        infobip.update_person(logger, person['id'], {
-            **person,
-            "customAttributes": {
-                "asesor_name":  activos[asesor_i][0],
-                "asesor_phone": activos[asesor_i][1]
+    for page in infobip.get_persons(logger, filtro):
+        for person in page:
+            asesor = {
+                "name":  activos[asesor_i][0],
+                "phone": activos[asesor_i][1]
             }
-        }) 
-        asesor_i += 1
-        asesor_i %= len(activos)
+            infobip.update_person(logger, person['id'], {
+                **person,
+                "customAttributes": {
+                    "asesor_name":  asesor['name'],
+                    "asesor_phone": asesor['phone']
+                }
+            }) 
+            lead = infobip.infobip2lead(person)
+            wpp.send_msg_asesor(asesor['phone'], lead)
+            asesor_i += 1
+            asesor_i %= len(activos)
